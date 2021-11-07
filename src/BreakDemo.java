3b4947851e72fb7879f75c65a948984cdde1e655
Sizertt <93645062+Sizertt@users.noreply.github.com>
2021-11-07 09:20:02 +0300

    BreakDemo

---
+++ src/BreakDemo.java
@@ -0,0 +1,16 @@
+public class BreakDemo {
+    public static void main(String[] args) {
+
+        int nmax = 25;
+        for (int n = 0; n < nmax; n++) {
+
+        /* Цикл выполняется до тех пор, пока
+        Квадрат числа n не станет больше 25 */
+            if ((n * n) >= nmax) break;
+            System.out.println("n равно " + n + "n в квадрате равно " + n * n);
+        }// for
+            System.out.println("Конец цикла");
+
+        } // main(String[]) method
+    } // BreakDemo class
+
